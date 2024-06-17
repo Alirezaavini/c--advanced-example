19e5f415e11aeaebc0d3c8b82d9e86465b92f390
AlirezaAvini <alirezaavini751@gmail.com>
2024-06-17 10:46:24 +0330

    add ef core

--- DotNet8/EntityFramework/NutShellDBContext.cs
+++ DotNet8/EntityFramework/NutShellDBContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace DotNet8.EntityFramework
 {
-    public class NutShellDBContext 
+    public class NutShellDBContext : DbContext
     {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(@"Server=(local);Database=DOTNET8;Trusted_Connection=True");
+
+
     }
 }
--- DotNet8/obj/Debug/net8.0/DotNet8.AssemblyInfo.cs
+++ DotNet8/obj/Debug/net8.0/DotNet8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cdf65ff2de0637dd7621da9878fb12548b441dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ad56deb806f6b7e157e0400050a44146b7c4cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
